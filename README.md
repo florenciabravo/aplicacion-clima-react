c462d02cc645f8cde177cb2076626a3aa7b93f92
florenciabravo <florenciabravonovillo@gmail.com>
2024-10-04 21:35:31 -0300

    Realizamos una aplicacion de clima usando una API y CSS comun sin librerias externas

---
+++ README.md
@@ -0,0 +1,14 @@
+# Aplicación de Clima
+Este repositorio contiene un proyecto en REACT JS de clima, donde se puede buscar por ciudad, parte del curso de REACT.JS en Digital House.
+
+# Uso
+- Clone el repositorio haciendo git clone https://github.com/florenciabravo/aplicacion-clima-react
+- Genere la API key del sitio: Open Weather https://openweathermap.org
+- Abra el proyecto en su editor de código y en la carpeta ./project-react-app/src actualice el componente WeatherApp.jsx en linea 10 con su API key generada
+- Luego en la terminal sobre la carpeta del proyecto ingrese npm install
+- Una vez finalizado podrá ejecutar el proyecto en formato desarrollo usando npm run dev
+# Requiere:
+NODE.JS: Debe tener instalado NodeJS
+GIT: Debe tener Instalado GIT
+# Acerca del curso de React.JS
+En este curso aprenderás la tecnología frontend más buscada del mercado, la cual te permite realizar increíbles aplicaciones webs.
---
+++ src/WeatherApp.jsx
@@ -0,0 +1,60 @@
+import React, { useState } from 'react'
+import './WeatherApp.css'
+
+export const WeatherApp = () => {
+
+    const [city, setCity] = useState('')
+    const [weatherData, setWeatherData] = useState(null)
+
+    const urlBase = 'https://api.openweathermap.org/data/2.5/weather'
+    const API_KEY = 'YOUR API KEY'
+    const difkelvin = 273.15 // Para lograr obtener grados Celsious debemos restar este nro a los grados kelvin
+
+    const fetchWeatherData = async () => {
+        try {
+            const response = await fetch(`${urlBase}?q=${city}&appid=${API_KEY}&lang=es`)
+            const data = await response.json()
+            setWeatherData(data)
+        } catch (error) {
+            console.error('Ha habido un error: ', error)
+        }
+    }
+
+    const handleCityChange = (event) => {
+        setCity(event.target.value)
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        fetchWeatherData()
+    }
+    return (
+        <div className='container'>
+            <h1>Aplicacion de Clima</h1>
+            <form onSubmit={handleSubmit}>
+                <input
+                    type='text'
+                    placeholder='Ingrese una ciudad'
+                    value={city}
+                    onChange={handleCityChange}
+                />
+                <button type='submit'>Buscar</button>
+            </form>
+
+            {weatherData && (
+
+                <div>
+                    <h2>{weatherData.name}, {weatherData.sys.country}</h2>
+                    <p>La temperatura actual es {Math.floor(weatherData.main.temp - difkelvin)}C</p>
+                    <p>La condicion meteorologica actual: {weatherData.weather[0].description}</p>
+                    <img
+                        src={`https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`}
+                        alt={weatherData.weather[0].description}
+                    />
+                </div>
+            )
+
+            }
+        </div>
+    )
+}
